ed0aac8bc11306097702b4fbe36f184a18f7dc83
BenjaminCoupry <56168210+BenjaminCoupry@users.noreply.github.com>
2020-12-17 12:41:46 +0100

    mini debug

--- hidoop/src/hdfs/utils/ServeurNommage.java
+++ hidoop/src/hdfs/utils/ServeurNommage.java
@@ -5,16 +5,15 @@ import java.rmi.registry.LocateRegistry;
 
 public class ServeurNommage {
     //arg[0] = dossier ou le service de Nommage enregistre les fragments
-    //arg[1] = adresse pour acceder au serveur de Nommage
     //arg[2] = port
     public static void main(String args[]) {
         try {
-            int port = Integer.parseInt(args[2]);
+            int port = Integer.parseInt(args[1]);
             LocateRegistry.createRegistry(port);
             // Create an instance of the server object
             Nommage nommage = new NommageHardDisk(args[0]);
             // Register the object with the naming service
-            Naming.rebind(args[1]+":"+port+"/serviceNommageFragments", nommage);
+            Naming.rebind("/serviceNommageFragments", nommage);
             System.out.println(" bound in registry");
         } catch (Exception exc) {System.out.println(exc.getMessage()); }
     }
